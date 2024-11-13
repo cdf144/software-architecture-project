e843987f46a8c4783c891a7d05189e4307f9c799
Trần Đình Phú <56244176+cdf144@users.noreply.github.com>
2024-11-13 21:53:16 +0700

    chore: reformatted code

--- models.js
+++ models.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
-const { PostgresDialect } = require('@sequelize/postgres');
-const { Sequelize, DataTypes } = require('sequelize');
+require("dotenv").config();
+const { PostgresDialect } = require("@sequelize/postgres");
+const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize(
-    process.env.PGDATABASE, 
-    process.env.PGUSER, 
-    process.env.PGPASSWORD, 
-    {
-        host: process.env.PGHOST,
-        dialect: 'postgres',
-        port: process.env.PGPORT,
-    },
+  process.env.PGDATABASE,
+  process.env.PGUSER,
+  process.env.PGPASSWORD,
+  {
+    host: process.env.PGHOST,
+    dialect: "postgres",
+    port: process.env.PGPORT,
+  },
 );
 
 // const sequelize = new Sequelize(
@@ -29,27 +29,26 @@ const sequelize = new Sequelize(
 //     database: process.env.POSTGRES_DB,
 // });
 
-
-const UrlShortener = sequelize.define('UrlShortener', {
-    id: {
-        type: DataTypes.STRING(5),
-        primaryKey: true
-    },
-    url: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-
+const UrlShortener = sequelize.define("UrlShortener", {
+  id: {
+    type: DataTypes.STRING(5),
+    primaryKey: true,
+  },
+  url: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
 
-sequelize.sync()
-    .then(() => {
-        console.log('Database & table created!');
-    })
-    .catch(err => {
-        console.error('Error creating database:', err);
-    });
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Database & table created!");
+  })
+  .catch((err) => {
+    console.error("Error creating database:", err);
+  });
 
 module.exports = {
-    UrlShortener
+  UrlShortener,
 };
--- utils.js
+++ utils.js
@@ -1,92 +1,92 @@
-const { UrlShortener } = require('./models');
-const redis = require('redis');
+const { UrlShortener } = require("./models");
+const redis = require("redis");
 
 const redisClient = redis.createClient();
-redisClient.on('error', err => console.log('Redis Client Error', err));
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
 redisClient.connect();
 
-
 function makeID(length) {
-    while (true) {
-        let result = '';
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        const charactersLength = characters.length;
-        let counter = 0;
-        while (counter < length) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-            counter += 1;
-        }
-        if (UrlShortener.findOne({ where: { id: id } }) !== null) {
-            return result;
-        }
+  while (true) {
+    let result = "";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
     }
+    if (UrlShortener.findOne({ where: { id: id } }) !== null) {
+      return result;
+    }
+  }
 }
 
 async function findOrigin(id) {
-    try {
-        // Check Redis first
-        const cached = await redisClient.hGet('id to url', id);
-        if (cached != null) {
-            console.log("Cache hit");
-            return cached;
-        }
-        console.log("Cache miss");
-
-        const record = await UrlShortener.findOne({ where: { id: id } });
-        if (record) {
-            await redisClient.hSet('id to url', id, record.url, { EX: 43200 });
-            return record.url;
-        } else {
-            return null;
-        }
-    } catch (err) {
-        console.error('Error finding original URL:', err);
-        return null;
+  try {
+    // Check Redis first
+    const cached = await redisClient.hGet("id to url", id);
+    if (cached != null) {
+      console.log("Cache hit");
+      return cached;
     }
+    console.log("Cache miss");
+
+    const record = await UrlShortener.findOne({ where: { id: id } });
+    if (record) {
+      await redisClient.hSet("id to url", id, record.url, { EX: 43200 });
+      return record.url;
+    } else {
+      return null;
+    }
+  } catch (err) {
+    console.error("Error finding original URL:", err);
+    return null;
+  }
 }
 
 async function shortUrl(url) {
-    let originUrl = {id: await redisClient.hGet('url to id', url)};
+  let originUrl = { id: await redisClient.hGet("url to id", url) };
 
-    if (!originUrl.id) {
-        console.log("Cache miss!");
-        originUrl = await UrlShortener.findOne({ where: { url: url } });
-        await redisClient.hSet('id to url', originUrl.id, url, { EX: 43200 });
-        await redisClient.hSet('url to id', url, originUrl.id, { EX: 43200 });
-    } else {
-        console.log("Cache hit!");
-    }
-    if (originUrl) {
-        console.log('URL already exists:');
-        return originUrl.id;
-    }
-    
-    let newID = makeID(5);
-    try {
-        await UrlShortener.create({ id: newID, url: url });
-        await redisClient.hSet('id to url', newID, url, { EX: 43200 });
-        await redisClient.hSet('url to id', url, newID, { EX: 43200 });
-        return newID;
-    } catch (err) {
-        console.error('Error creating shortened URL:', err);
-        throw err;
-    }
+  if (!originUrl.id) {
+    console.log("Cache miss!");
+    originUrl = await UrlShortener.findOne({ where: { url: url } });
+    await redisClient.hSet("id to url", originUrl.id, url, { EX: 43200 });
+    await redisClient.hSet("url to id", url, originUrl.id, { EX: 43200 });
+  } else {
+    console.log("Cache hit!");
+  }
+  if (originUrl) {
+    console.log("URL already exists:");
+    return originUrl.id;
+  }
+
+  let newID = makeID(5);
+  try {
+    await UrlShortener.create({ id: newID, url: url });
+    await redisClient.hSet("id to url", newID, url, { EX: 43200 });
+    await redisClient.hSet("url to id", url, newID, { EX: 43200 });
+    return newID;
+  } catch (err) {
+    console.error("Error creating shortened URL:", err);
+    throw err;
+  }
 }
 
 async function getAllUrls() {
-    try {
-        const records = await UrlShortener.findAll({
-            attributes: ['id', 'url'],
-        });
-        return records.map(record => record.get({ plain: true }));
-    } catch (err) {
-        console.error("Error fetching URLs:", err);
-        throw err;
-    }
+  try {
+    const records = await UrlShortener.findAll({
+      attributes: ["id", "url"],
+    });
+    return records.map((record) => record.get({ plain: true }));
+  } catch (err) {
+    console.error("Error fetching URLs:", err);
+    throw err;
+  }
 }
 
 module.exports = {
-    findOrigin,
-    shortUrl,
-    getAllUrls
-}
+  findOrigin,
+  shortUrl,
+  getAllUrls,
+};
